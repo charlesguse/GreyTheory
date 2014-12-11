9b5c64ed3d88f8c514621fec1b8a55b5a25f3de0
Charlie <Charlie@localhost>
2014-12-11 11:24:43 -0500

    Initial commit. Added the ignore file and a reference to the linear algebra library that I want to use.

---
+++ GreyTheory/GreyTheory/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyTheory
+{
+    public class Class1
+    {
+    }
+}
